2230a789fefc8e18a3a1dce7b87317bceaf947b9
Shahith R <87928073+Shahithraj@users.noreply.github.com>
2021-09-18 02:08:00 +0530

    Add files via upload

---
+++ countdown.js
@@ -0,0 +1,73 @@
+const element = document.querySelector(".countdown")
+// let sec = 10;
+// setInterval(()=>{
+//   if(sec==0){
+//     element.innerHTML = "HAPPY NEW YEAR";
+//     element.style.color="red"
+//   }
+//   if(sec<0){
+//     sec = 10;
+//     element.style.color="black"
+//   }
+//   if(sec>0){
+//  element.innerHTML = sec;
+//  element.style.color="black"
+//   }
+//     sec--;
+  
+// },1000);
+setTimeout(()=>{
+   element.innerText = 10;
+// },1000)
+setTimeout(()=>{
+  element.innerText = 9;
+// },2000)
+setTimeout(()=>{
+  element.innerText = 8;
+// },3000)
+setTimeout(()=>{
+  element.innerText = 7;
+// },4000)
+setTimeout(()=>{
+  element.innerText = 6;
+// },5000)
+setTimeout(()=>{
+  element.innerText = 5;
+// },6000)
+setTimeout(()=>{
+  element.innerText = 4;
+// },7000)
+setTimeout(()=>{
+  element.innerText = 3;
+// },8000)
+setTimeout(()=>{
+  element.innerText = 2;
+// },9000)
+setTimeout(()=>{
+  element.innerText = 1;
+// },10000)
+setTimeout(()=>{
+  element.innerText = "Happy New Year";
+// },11000)
+// },1000)
+// },1000)
+// },1000)
+// },1000)
+// },1000)
+// },1000)
+// },1000)
+// },1000)
+// },1000)
+// },1000)
+// },5000)
+},11000)
+},10000)
+},9000)
+},8000)
+},7000)
+},6000)
+},5000)
+},4000)
+},3000)
+},2000)
+},1000);
